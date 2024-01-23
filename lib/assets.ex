59372a96a075c878ff84d8be4b06d29321624245
briankariuki <briankariukidev@gmail.com>
2024-01-23 23:38:38 +0300

    add select

--- lib/assets.ex
+++ lib/assets.ex
@@ -150,4 +150,32 @@ defmodule Tremorx.Assets do
     </svg>
     """
   end
+
+  attr(:rest, :global)
+
+  @doc """
+  Renders a arrow down head icon
+  """
+  def arrow_down_head_icon(assigns) do
+    ~H"""
+    <svg {@rest} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+      <path d="M11.9999 13.1714L16.9497 8.22168L18.3639 9.63589L11.9999 15.9999L5.63599 9.63589L7.0502 8.22168L11.9999 13.1714Z">
+      </path>
+    </svg>
+    """
+  end
+
+  attr(:rest, :global)
+
+  @doc """
+  Renders a x circle icon
+  """
+  def x_circle_icon(assigns) do
+    ~H"""
+    <svg {@rest} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+      <path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 10.5858L9.17157 7.75736L7.75736 9.17157L10.5858 12L7.75736 14.8284L9.17157 16.2426L12 13.4142L14.8284 16.2426L16.2426 14.8284L13.4142 12L16.2426 9.17157L14.8284 7.75736L12 10.5858Z">
+      </path>
+    </svg>
+    """
+  end
 end
